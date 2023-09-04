800d531edf233dfd08c04ca68973623d4874e637
SirJShostakiv <sshv2013@gmail.com>
2023-09-04 19:36:58 +0500

    All done. Test successful

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,28 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean("cat")
+    public Cat getCat() {
+        return new Cat();
+    }
+
+    @Bean("dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+
+    @Bean("timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
